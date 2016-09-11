a7f69b990a4a67df6460fd3d19032c945bce3ce9
Timi Aiyemo <timi@m33dlabs.com>
2016-09-11 23:21:00 +0100

    Secret in config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port : process.env.PORT || 4315,
     database : "db",
-    secret : "juwvovbskvbw16t237r283982y2392SKLABWVBVKSJK9qurwiefhweofw#@%^!(*#^^^!&@*()@)__!_#(&#^@"
+    secret : "secret"
 }
